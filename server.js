bb7d6b3f9ed1744e6aae9f74eb8bedac8c2c316c
Michael <michael138leighton@gmail.com>
2022-05-26 13:11:09 -0500

    more changes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
 
 app.get('/', (req, res) => res.send('hello!'));
+
+io.on('connection', (socket) => {  
+    console.log('a user connected'); 
+ });
+ 
   http.listen(3000, () => {
   console.log('http://localhost:3000/');
 });
+
